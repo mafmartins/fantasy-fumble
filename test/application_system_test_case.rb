c279d680a4818dbbafa6d21d6ce908a42c24f7e5
mafmartins <marcio.af.martins@gmail.com>
2025-01-24 14:49:47 +0000

    Enhance Selenium driver options for improved stability in system tests

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,8 +1,10 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ], options: {
-    browser: :remote,
-    url: "http://#{ENV.fetch("SELENIUM_HOST", "localhost")}:4444"
-  }
+  # Reference: https://stackoverflow.com/questions/53902507/unknown-error-session-deleted-because-of-page-crash-from-unknown-error-cannot
+  driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ], options: { browser: :remote, url: "http://#{ENV.fetch("SELENIUM_HOST", "localhost")}:4444/wd/hub" }  do |driver_options|
+    driver_options.add_argument("--disable-dev-shm-usage")
+    driver_options.add_argument("--no-sandbox")
+    driver_options.add_argument("--disable-gpu")
+  end
 end
