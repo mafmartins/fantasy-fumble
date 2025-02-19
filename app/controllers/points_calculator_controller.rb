3dc29a678bf3675eebb823e86eff8cd32232b231
mafmartins <marcio.af.martins@gmail.com>
2025-02-19 11:51:04 +0000

    Implement points calculation logic and add success view; enhance controller tests

--- app/controllers/points_calculator_controller.rb
+++ app/controllers/points_calculator_controller.rb
@@ -8,6 +8,31 @@ class PointsCalculatorController < ApplicationController
   end
 
   def calculate
-    debugger
+    athletes_ids = params[:athletes]
+    season_id = params[:season]
+    week_id = params[:week]
+
+    if athletes_ids.nil? || athletes_ids.empty? || season_id.nil? || week_id.nil?
+      render json: { error: "Invalid params" }, status: :unprocessable_entity
+      return
+    end
+
+    athletes = Athlete.where(id: athletes_ids)
+    if athletes.empty? || athletes.length != athletes_ids.length
+      render json: { error: "Athletes not found" }, status: :not_found
+      return
+    end
+
+    athletes = athletes.map do |athlete|
+      {
+        id: athlete.id,
+        full_name: athlete.full_name,
+        display_name: athlete.display_name,
+        position: athlete.position.name,
+        team: athlete.team.display_name,
+        headshot: athlete.headshot,
+        points: 0
+      }
+    end
   end
 end
--- test/controllers/points_calculator_controller_test.rb
+++ test/controllers/points_calculator_controller_test.rb
@@ -3,11 +3,23 @@ require "test_helper"
 class PointsCalculatorControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:user_one)
+    @athletes = [ athletes(:one), athletes(:two) ]
   end
   test "should get index" do
     sign_in_as @user
 
     get points_calculator_index_url
+    assert_response :success
+  end
+
+  test "should calculate points" do
+    sign_in_as @user
+
+    post points_calculator_calculate_url, params: {
+      athletes: [ @athletes[0].id, @athletes[1].id ],
+      season: "2025",
+      week: "1"
+    }
     assert_response :success
   end
 end
